7f5c7c1c47a96b32931c596e46a8b58b865bae18
Muhammad Abdullah <mabullah20@gmail.com>
2024-06-27 00:44:05 +0500

    Add the pixel-size button and add the information section of the webpage

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-let container = document.querySelector(".container");
 let SQUARES_DIM = 32;
-for(let i =0; i <SQUARES_DIM**2;i++){
-    console.log(container.clientWidth);
-    let div = document.createElement("div");
-    div.style.cssText=`width:${(container.clientWidth)/(SQUARES_DIM)-0.1}px;height:${(container.clientWidth)/(SQUARES_DIM)-0.1}px;`;
-    div.classList.add("box");
-    div.id = `box${i}`;
-    container.appendChild(div);
+function createGrid(){
+    let container = document.querySelector(".container");
+    for(let i =0; i <SQUARES_DIM**2;i++){
+        console.log(container.clientWidth);
+        let div = document.createElement("div");
+        div.style.cssText=`width:${(container.clientWidth)/(SQUARES_DIM)-0.1}px;height:${(container.clientWidth)/(SQUARES_DIM)}px;`;
+        div.classList.add("box");
+        div.id = `box${i}`;
+        container.appendChild(div);
+    }
+    container.addEventListener('mouseover',(event)=>{
+        console.log(event.target.id)
+        let box = document.getElementById(event.target.id);
+        box.style.backgroundColor="black";
+    });
+    container.addEventListener('mouseout',(event)=>{
+        let box = document.getElementById(event.target.id);
+        box.style.backgroundColor="gray";
+    });
 }
-let boxes = document.querySelectorAll(".box");
-container.addEventListener('mouseover',(event)=>{
-    let box = document.getElementById(event.target.id);
-    box.style.backgroundColor="black";
-});
-container.addEventListener('mouseout',(event)=>{
-    let box = document.getElementById(event.target.id);
-    box.style.backgroundColor="gray";
-});
+createGrid()
+function popup(){
+    let container = document.querySelector(".container");
+    container.remove();
+    let new_container = document.createElement("div");
+    new_container.classList.add("container");
+    let body = document.querySelector("body");
+    body.insertBefore(new_container,body.firstChild);
+
+    let pixel_size=window.prompt("Enter the Pixel Size");
+    SQUARES_DIM = pixel_size;
+    createGrid();
+    
+
+    // createGrid();
+}
