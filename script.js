13e81bdb5c6056b7122bf45b69c8e7d81fcc80c0
Muhammad Abdullah <mabullah20@gmail.com>
2024-06-27 01:34:19 +0500

    Add the get Funky feature which makes the etch-a-sketch board colourful

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let SQUARES_DIM = 16;
-function createGrid(){
+function createGrid(funky = false){
     let container = document.querySelector(".container");
     for(let i =0; i <SQUARES_DIM**2;i++){
         console.log(container.clientWidth);
@@ -11,10 +11,16 @@ function createGrid(){
     }
     container.addEventListener('mouseover',(event)=>{
         let box = document.getElementById(event.target.id);
-        box.style.backgroundColor="black";
+        let rand1=Math.floor(Math.random()*255);
+        let rand2=Math.floor(Math.random()*255);
+        let rand3=Math.floor(Math.random()*255);
+        box.style.backgroundColor= funky? `rgb(${rand1},${rand2},${rand3})`:"black";
+        box.classList.toggle("cursor");
+        
     });
     container.addEventListener('mouseout',(event)=>{
         let box = document.getElementById(event.target.id);
+        box.classList.toggle("cursor");
         box.style.opacity = `${parseFloat(window.getComputedStyle(box).opacity)+0.1}`;
     });
 }
@@ -33,4 +39,14 @@ function popup(){
     
 
     // createGrid();
+}
+
+function getFunky(){
+    let container = document.querySelector(".container");
+    container.remove();
+    let new_container = document.createElement("div");
+    new_container.classList.add("container");
+    let body = document.querySelector("body");
+    body.insertBefore(new_container,body.firstChild);
+    createGrid(true);
 }
